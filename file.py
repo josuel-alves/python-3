14d70e6b8b2b399f887bf7cf63c969f7701f10e5
josuel-alves <josuel.alves1@gmail.com>
2020-07-27 21:46:09 -0300

    Project Python 3.8.5

--- file.py
+++ file.py
@@ -1,2 +1,14 @@
-# Teste
-print('Test')
+# Start
+nameCliente = 'Enter your name:'
+print('Be welcome {}'.format(nameCliente))
+
+# Itens
+itemOne = 'Item 01'
+itemTwo = 'Item 02'
+itemTree = 'Item 03'
+
+print('Itens')
+print(itemOne, itemTwo, itemTree)
+
+# Select Itens
+select = input('Select your item')
