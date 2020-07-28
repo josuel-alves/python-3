056821c873729f62d424b22f637e05d85ce233a6
josuel-alves <josuel.alves1@gmail.com>
2020-07-27 22:06:21 -0300

    Project Python 3.8.5

--- file.py
+++ file.py
@@ -1,6 +1,6 @@
 # Start
 nameCliente = 'Enter your name:'
-print('Be welcome {}!'.format(nameCliente))
+print('Be welcome {:=^50}!'.format(nameCliente))
 
 # Itens
 term = 'Item'
@@ -8,7 +8,8 @@ itemOne = int(1)
 itemTwo = int(2)
 itemTree = int(3)
 
-print(term, ' {}, {} or {}'.format(itemOne, itemTwo, itemTree))
+print(term, ' {:*^5}, {:*^5} or {:*^5}'.format(itemOne, itemTwo, itemTree))
+# Item **1**, **2** or **3**
 
 # Select Itens
 select = input('Select your item')
